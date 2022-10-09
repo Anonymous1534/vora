1bbc7972fcbc7e8b36a2fd5b0f05504996050ba0
Boonyarit (Jay) Wongsombut <broadprogame@gmail.com>
2022-10-09 14:40:27 +0700

    improve overview page

--- front-end/src/components/Overviews/index.tsx
+++ front-end/src/components/Overviews/index.tsx
@@ -25,7 +25,7 @@ const Overviews = () => {
       plate: "V430303",
       routeFrom: "Barcelana",
       routeTo: "Valencia",
-      process: 25,
+      process: 18,
     },
   ];
 
@@ -569,14 +569,202 @@ const Overviews = () => {
         </div>
         <div className="ocb-card">
           <div className="ocb-header">
-            <div className="ocb-title">Delayed delivery</div>
+            <div className="ocb-title">Recent requests</div>
             <div className="ocb-more">
               <Link to="/">Show all</Link>
             </div>
           </div>
           <div className="ocb-body">
-            <div className="ocb-table">
-              <DataTable columns={columns} data={data} />
+            <div className="ocb-request">
+              <div className="ocb-r-group">
+                <div className="ocb-r-left">
+                  <div className="ocb-rl-ico">
+                    <svg
+                      width="24px"
+                      height="24px"
+                      viewBox="0 0 512 512"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <polygon
+                        style={{ fill: "#E0B76E" }}
+                        points="376.811,187.88 256.003,512 29.001,384.007 29.001,127.997 "
+                      />
+                      <polygon
+                        style={{ fill: "#A38251" }}
+                        points="256.003,0.004 29.001,127.997 256.003,256.002 256.003,512 483.005,127.997 "
+                      />
+                      <polygon
+                        style={{ fill: "#F2CA7F" }}
+                        points="256.003,256.002 256.003,512 483.005,384.007 483.005,127.997 "
+                      />
+                      <polygon
+                        style={{ fill: "#898686" }}
+                        points="402.959,270.659 369.019,247.073 333.995,309.437 359.779,294.939 359.779,400.591 
+	377.722,390.471 377.722,284.849 "
+                      />
+                      <polygon
+                        style={{ fill: "#E0B76E" }}
+                        points="321.283,159.537 94.281,31.535 28.995,128.001 255.998,256.003 "
+                      />
+                      <polygon
+                        style={{ fill: "#F2CA7F" }}
+                        points="378.72,130.136 151.716,2.134 255.998,0 483.002,128.001 "
+                      />
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                    </svg>
+                  </div>
+                  <div className="ocb-rl-text">
+                    <div className="ocb-rl-title">Parcel redirection</div>
+                    <div className="ocb-rl-des">
+                      Destination Valencia - Barcelrona
+                    </div>
+                  </div>
+                </div>
+                <div className="ocb-r-right">
+                  <div className="ocb-rr-time">1 min ago</div>
+                </div>
+              </div>
+              <div className="ocb-r-group">
+                <div className="ocb-r-left">
+                  <div className="ocb-rl-ico">
+                    <svg
+                      width="24px"
+                      height="24px"
+                      viewBox="0 0 512 512"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <polygon
+                        style={{ fill: "#E0B76E" }}
+                        points="376.811,187.88 256.003,512 29.001,384.007 29.001,127.997 "
+                      />
+                      <polygon
+                        style={{ fill: "#A38251" }}
+                        points="256.003,0.004 29.001,127.997 256.003,256.002 256.003,512 483.005,127.997 "
+                      />
+                      <polygon
+                        style={{ fill: "#F2CA7F" }}
+                        points="256.003,256.002 256.003,512 483.005,384.007 483.005,127.997 "
+                      />
+                      <polygon
+                        style={{ fill: "#898686" }}
+                        points="402.959,270.659 369.019,247.073 333.995,309.437 359.779,294.939 359.779,400.591 
+	377.722,390.471 377.722,284.849 "
+                      />
+                      <polygon
+                        style={{ fill: "#E0B76E" }}
+                        points="321.283,159.537 94.281,31.535 28.995,128.001 255.998,256.003 "
+                      />
+                      <polygon
+                        style={{ fill: "#F2CA7F" }}
+                        points="378.72,130.136 151.716,2.134 255.998,0 483.002,128.001 "
+                      />
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                    </svg>
+                  </div>
+                  <div className="ocb-rl-text">
+                    <div className="ocb-rl-title">Parcel redirection</div>
+                    <div className="ocb-rl-des">
+                      Destination Valencia - Barcelrona
+                    </div>
+                  </div>
+                </div>
+                <div className="ocb-r-right">
+                  <div className="ocb-rr-time">1 min ago</div>
+                </div>
+              </div>
+              <div className="ocb-r-group">
+                <div className="ocb-r-left">
+                  <div className="ocb-rl-ico">
+                    <svg
+                      width="24px"
+                      height="24px"
+                      viewBox="0 0 512 512"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <polygon
+                        style={{ fill: "#E0B76E" }}
+                        points="376.811,187.88 256.003,512 29.001,384.007 29.001,127.997 "
+                      />
+                      <polygon
+                        style={{ fill: "#A38251" }}
+                        points="256.003,0.004 29.001,127.997 256.003,256.002 256.003,512 483.005,127.997 "
+                      />
+                      <polygon
+                        style={{ fill: "#F2CA7F" }}
+                        points="256.003,256.002 256.003,512 483.005,384.007 483.005,127.997 "
+                      />
+                      <polygon
+                        style={{ fill: "#898686" }}
+                        points="402.959,270.659 369.019,247.073 333.995,309.437 359.779,294.939 359.779,400.591 
+	377.722,390.471 377.722,284.849 "
+                      />
+                      <polygon
+                        style={{ fill: "#E0B76E" }}
+                        points="321.283,159.537 94.281,31.535 28.995,128.001 255.998,256.003 "
+                      />
+                      <polygon
+                        style={{ fill: "#F2CA7F" }}
+                        points="378.72,130.136 151.716,2.134 255.998,0 483.002,128.001 "
+                      />
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                      <g></g>
+                    </svg>
+                  </div>
+                  <div className="ocb-rl-text">
+                    <div className="ocb-rl-title">Parcel redirection</div>
+                    <div className="ocb-rl-des">
+                      Destination Valencia - Barcelrona
+                    </div>
+                  </div>
+                </div>
+                <div className="ocb-r-right">
+                  <div className="ocb-rr-time">1 min ago</div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
---
+++ readme.md
@@ -0,0 +1,1 @@
+https://www.behance.net/gallery/151463195/Wayels-Courier-System-Dashboard?tracking_source=search_projects%7Ctrendy%20dashboard
